a87e079b9fde589b755da3d0429cc4b718377cf9
tretia2 <anton.tretiak1@nure.ua>
2022-07-01 14:17:48 +0300

    run but stop

--- app/src/main/java/com/example/ratingapp/screens/favourite/FavouriteFragment.kt
+++ app/src/main/java/com/example/ratingapp/screens/favourite/FavouriteFragment.kt
@@ -1,114 +1,20 @@
-
 package com.example.ratingapp.screens.favourite
 
-import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.ListView
+
 import androidx.fragment.app.Fragment
-import com.example.ratingapp.Film
-import com.example.ratingapp.FilmAdapter
-import com.example.ratingapp.R
+
 
 class FavouriteFragment : Fragment() {
-    private var mMediaPlayer: MediaPlayer? = null
-
-
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val rootView: View = inflater.inflate(R.layout.word_list, container, false)
-
-
-        // Create a list of words
-        val films: ArrayList<Film> = ArrayList()
-        films.add(
-            Film(
-                R.string.family_father, R.string.miwok_family_father,
-                R.drawable.family_father
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_mother, R.string.miwok_family_mother,
-                R.drawable.family_mother
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_son, R.string.miwok_family_son,
-                R.drawable.family_son
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_daughter, R.string.miwok_family_daughter,
-                R.drawable.family_daughter
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_older_brother, R.string.miwok_family_older_brother,
-                R.drawable.family_older_brother
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_younger_brother, R.string.miwok_family_younger_brother,
-                R.drawable.family_younger_brother
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_older_sister, R.string.miwok_family_older_sister,
-                R.drawable.family_older_sister
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_younger_sister, R.string.miwok_family_younger_sister,
-                R.drawable.family_younger_sister
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_grandmother, R.string.miwok_family_grandmother,
-                R.drawable.family_grandmother
-            )
-        )
-        films.add(
-            Film(
-                R.string.family_grandfather, R.string.miwok_family_grandfather,
-                R.drawable.family_grandfather
-            )
-        )
-
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
-        val adapter = FilmAdapter(activity, films, R.color.category_family)
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
-        val listView = rootView.findViewById<View>(R.id.list) as ListView
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.adapter = adapter
-
-        // Set a click listener to play the audio when the list item is clicked on
-        listView.onItemClickListener =
-            OnItemClickListener { _, _, position, _ ->
-
-                val film: Film = films[position]
-
-            }
-        return rootView
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
 
--- app/src/main/java/com/example/ratingapp/screens/start/StartFragment.kt
+++ app/src/main/java/com/example/ratingapp/screens/start/StartFragment.kt
@@ -1,120 +1,19 @@
 package com.example.ratingapp.screens.start
 
-import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.ListView
 import androidx.fragment.app.Fragment
-import com.example.ratingapp.Film
-import com.example.ratingapp.FilmAdapter
-import com.example.ratingapp.R
 
 class StartFragment : Fragment() {
-    private var mMediaPlayer: MediaPlayer? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val rootView: View = inflater.inflate(R.layout.word_list, container, false)
-
-
-        val films: ArrayList<Film> = ArrayList()
-        films.add(
-            Film(
-                R.string.number_one, R.string.miwok_number_one,
-                R.drawable.number_one
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_two, R.string.miwok_number_two,
-                R.drawable.number_two
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_three, R.string.miwok_number_three,
-                R.drawable.number_three
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_four, R.string.miwok_number_four,
-                R.drawable.number_four
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_five, R.string.miwok_number_five,
-                R.drawable.number_five
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_six, R.string.miwok_number_six,
-                R.drawable.number_six
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_seven, R.string.miwok_number_seven,
-                R.drawable.number_seven
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_eight, R.string.miwok_number_eight,
-                R.drawable.number_eight
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_nine, R.string.miwok_number_nine,
-                R.drawable.number_nine
-            )
-        )
-        films.add(
-            Film(
-                R.string.number_ten, R.string.miwok_number_ten,
-                R.drawable.number_ten
-            )
-        )
-
-
-        val adapter = FilmAdapter(activity, films, R.color.category_numbers)
-
-
-        val listView = rootView.findViewById<View>(R.id.list) as ListView
-
-
-        listView.adapter = adapter
-
-        listView.onItemClickListener =
-            OnItemClickListener { _, _, position, _ ->
-
-                val film: Film = films[position]
-
-            }
-        return rootView
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    override fun onStop() {
-        super.onStop()
-
-        releaseMediaPlayer()
-    }
-
-
-    private fun releaseMediaPlayer() {
-        if (mMediaPlayer != null) {
-
-            mMediaPlayer!!.release()
-
-            mMediaPlayer = null
-        }
-    }
 }
